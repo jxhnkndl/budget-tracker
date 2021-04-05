c10557e2b8f1f20b5c0c8eefb952d57a8b9e856b
jxhnkndl <jkroyston@gmail.com>
2021-04-05 16:50:01 -0400

    Reconfigure database connection to check for production database first before looking for local dev database

--- server.js
+++ server.js
@@ -20,8 +20,10 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Establish database connection
-mongoose.connect('mongodb://localhost/budget', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false,
 });
 
