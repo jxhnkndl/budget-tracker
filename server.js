91e3b2947a26fd4da934aa449457bb2ca3b9ca49
jxhnkndl <jkroyston@gmail.com>
2021-04-05 16:36:24 -0400

    Convert static PORT number to dynamic PORT variable for deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const compression = require('compression');
 
 // Config PORT
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Init Express
 const app = express();
